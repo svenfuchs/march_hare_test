2ed7239327f967ff5ba8021a596c66c3e4c24083
Sven Fuchs <me@svenfuchs.com>
2013-09-13 16:50:12 +0200

    Update consumer.rb

--- consumer.rb
+++ consumer.rb
@@ -9,6 +9,10 @@ connection = MarchHare.connect user: 'travis', password: 'travis', vhost: 'travi
 
 channel = connection.create_channel.tap { |channel| channel.prefetch = 1 }
 queue = channel.queue(name, :durable => true)
+
+# messages published by producer.rb will only end up in the queue if i add this line.
+# curiously, i can't find anything like this in travis-worker, neither in master nor
+# the branch docker-in-da-house that i'm currently using in my test setup.
 queue.bind(name, routing_key: name)
 
 queue.subscribe(ack: true, blocking: false) do |meta, payload|
